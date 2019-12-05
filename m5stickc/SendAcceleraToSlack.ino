76bb5f7aa5066d1f330004a3717575592c56caa3
odrum428 <oxkeita@gmail.com>
2019-12-05 23:30:44 +0900

    加速度を画面上に正しく表示させるところまで実装

--- m5stickc/SendAcceleraToSlack.ino
+++ m5stickc/SendAcceleraToSlack.ino
@@ -8,16 +8,18 @@ float accZ_g = 0;
 void setup() {
   // Initialize the M5StickC object
   M5.begin();
+  //初期設定開始 LED ON
+  pinMode(10, OUTPUT);
   // 6軸センサ初期化
   M5.MPU6886.Init();
   // LCD display
   M5.Lcd.setRotation(1);  // ボタンBが上になる向き
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextSize(1);
 }
 
 void loop() {
   M5.MPU6886.getAccelData(&accX_g,&accY_g,&accZ_g);
+  M5.Lcd.setCursor(0, 30);
   M5.Lcd.printf("Acc : %.2f  %.2f  %.2f   ", accX_g, accY_g, accZ_g);
   delay(100);
 }
