8048a56e725a45072728b6d60128313ba28347c8
odrum428 <oxkeita@gmail.com>
2019-12-05 23:12:38 +0900

    センサの値を画面に表示できることを確認

---
+++ m5stickc/SendAcceleraToSlack.ino
@@ -0,0 +1,23 @@
+#include <M5StickC.h>
+
+// 加速度
+float accX_g = 0;
+float accY_g = 0;
+float accZ_g = 0;
+
+void setup() {
+  // Initialize the M5StickC object
+  M5.begin();
+  // 6軸センサ初期化
+  M5.MPU6886.Init();
+  // LCD display
+  M5.Lcd.setRotation(1);  // ボタンBが上になる向き
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(1);
+}
+
+void loop() {
+  M5.MPU6886.getAccelData(&accX_g,&accY_g,&accZ_g);
+  M5.Lcd.printf("Acc : %.2f  %.2f  %.2f   ", accX_g, accY_g, accZ_g);
+  delay(100);
+}
