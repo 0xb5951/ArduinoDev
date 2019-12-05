1f3b15515ea5a142433cc5a95df98be1b280cd84
odrum428 <oxkeita@gmail.com>
2019-12-06 01:44:43 +0900

    加速度が一定以上になると光る仕組みを実装

--- m5stickc/SendAcceleraToSlack.ino
+++ m5stickc/SendAcceleraToSlack.ino
@@ -1,9 +1,14 @@
 #include <M5StickC.h>
 
-// 加速度
+// 加速度(単位が重力加速度[g])
 float accX_g = 0;
 float accY_g = 0;
 float accZ_g = 0;
+
+// 加速度(単位が加速度[m/s2])
+float accX = 0;
+float accY = 0;
+float accZ = 0;
 
 // 角速度
 float GyroX = 0;
@@ -13,20 +18,29 @@ float GyroZ = 0;
 void setup() {
   // Initialize the M5StickC object
   M5.begin();
-  //初期設定開始 LED ON
-//   pinMode(10, OUTPUT);
   // 6軸センサ初期化
   M5.MPU6886.Init();
+  // LED初期化   
+  pinMode(10, OUTPUT);
   // LCD display
   M5.Lcd.setRotation(1);  // ボタンBが上になる向き
   M5.Lcd.fillScreen(BLACK);
+  digitalWrite(10, HIGH);
 }
 
 void loop() {
+  digitalWrite(10, HIGH);
   M5.MPU6886.getAccelData(&accX_g,&accY_g,&accZ_g);
   M5.MPU6886.getGyroData(&GyroX,&GyroY,&GyroZ);
   M5.Lcd.setCursor(0, 30);
-  M5.Lcd.printf("Acc : %.2f  %.2f  %.2f   ", accX_g * 9.8, (float)accY_g * 9.8, (float)accZ_g * 9.8);
+  accX = accX_g * 9.8;
+  accY = accY_g * 9.8;
+  accZ = accZ_g * 9.8;
+  M5.Lcd.printf("Acc : %.2f  %.2f  %.2f   ", accX, accY, accZ);
   M5.Lcd.printf("Gyro : %.2f  %.2f  %.2f   ", GyroX, GyroY, GyroZ);
-  delay(100);
+
+  if (abs(accX + accY + accZ) > 19.6) {
+    digitalWrite(10, LOW);
+  }
+  delay(500);
 }
