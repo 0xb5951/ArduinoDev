b252ce3360914bf8aeef3675490540372adfa77b
odrum428 <oxkeita@gmail.com>
2019-12-24 01:20:24 +0900

    リファクタ

--- m5stickv/connect_to_m5stickc_and_v/uart.py
+++ m5stickv/connect_to_m5stickc_and_v/uart.py
@@ -4,28 +4,32 @@ from Maix import GPIO
 from fpioa_manager import fm, board_info
 from machine import UART
 
-#M5StickV Camera Start
-clock = time.clock()
+
+'''
+function define
+'''
+def camera_setup():
+    lcd.direction(lcd.YX_LRUD)
+    sensor.reset()
+    sensor.set_framesize(sensor.QQVGA)
+    sensor.set_pixformat(sensor.GRAYSCALE)
+    sensor.run(1)
+    sensor.skip_frames(time = 2000)
+
+'''
+main function
+'''
 lcd.init()
-sensor.reset()
-sensor.set_framesize(sensor.QQVGA)
-sensor.set_pixformat(sensor.GRAYSCALE)
-sensor.run(1)
-sensor.skip_frames(time = 2000)
-# LCD direction controll
-lcd.direction(lcd.YX_LRUD)
+camera_setup()
 
 #M5StickV GPIO_UART
 fm.register(35, fm.fpioa.UART2_TX, force=True)
 fm.register(34, fm.fpioa.UART2_RX, force=True)
 fm.register(board_info.BUTTON_A, fm.fpioa.GPIO5, force=True)
 
-uart_Port = UART(UART.UART2, 115200,8,0,0, timeout=1000, read_buf_len= 4096)
-
-print(board_info.pin_map())
-
 #M5StickV main button
 main_button = GPIO(GPIO.GPIO5, GPIO.IN, GPIO.PULL_UP)
+uart_Port = UART(UART.UART2, 115200,8,0,0, timeout=1000, read_buf_len= 4096)
 
 while True:
     img = sensor.snapshot()
@@ -42,7 +46,8 @@ while True:
         uart_Port.write(img_buf)
         time.sleep(1)
 
-#   Send UART End
+# Send UART End
 uart_Port.deinit()
 del uart_Port
 print("finish")
+
