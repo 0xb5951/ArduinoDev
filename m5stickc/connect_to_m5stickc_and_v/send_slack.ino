bd64b4331d633776f3d7db826c2cab72e284ad8f
odrum428 <oxkeita@gmail.com>
2019-12-27 02:20:27 +0900

    不要な部分を削除

--- m5stickc/connect_to_m5stickc_and_v/send_slack.ino
+++ m5stickc/connect_to_m5stickc_and_v/send_slack.ino
@@ -1,19 +1,14 @@
-#include <ssl_client.h>
-#include <WiFiClientSecure.h>
 
-const char* Host = "https://slack.com/api/";
 size_t BUF_SIZE = 1024;
+int httpCode = 404;
 
 void send_slack(uint8_t* image_data, size_t image_sz) {
   const char* slack_bot_token = get_slack_bot_token();
   const char* oauth_token = get_oauth_token();
 
-  String path = "files.upload";
-  String url = Host + path;
-
   WiFiClientSecure client;
 
-  if (!client.connect(url, 443)) {
+  if (!client.connect("notify-api.line.me", 443)) {
     Serial.println("connection failed");
     return;
   }
@@ -25,70 +20,63 @@ void send_slack(uint8_t* image_data, size_t image_sz) {
 
   // header set
   client.print("POST " + url + " HTTP/1.1");
-  client.println("Host: " + (String)Host);
-  client.println("User-Agent: M5Stack");
-  client.println("Connection: close");
-  client.println("Content-Type: multipart/form-data;");
-  client.println("Authorization: Bearer " + slack_bot_token);
-  client.println("Content-Length: " + image_sz);
-  client.print(header);
-  
-  // body
-  String body = "token=" + oauth_token + "&channel=" + "#t_mizushima" + "&initial_comment=From M5stickV!";
-  client.print(body);
-
-  client.write(image_data, BUF_SIZE);
+  client.print("Host: " + (String)Host);
+  client.print("User-Agent: M5Stack");
+  client.print("Connection: close");
+  client.print("Content-Type: multipart/form-data; boundary=----WebKitFormBoundarySXkTMrIra0APbTor");
+  client.print("Authorization: Bearer " + String(slack_bot_token));
+  client.print("Content-Length: " + String(image_sz));
 
   bool Success_h = false;
   uint8_t line_try = 3;
-
-  size_t BUF_SIZE = 1024;
-  if ( image_data != NULL) {
-    
-    uint8_t *temp = image_data;
-    size_t sz = image_sz;
-
-    // バッファサイズごとに記述
-    while ( temp != NULL && sz) {
-      if ( sz >= BUF_SIZE) {
-        client.write(temp, BUF_SIZE);
-        temp += BUF_SIZE; sz -= BUF_SIZE;
-      } else {
-        client.write(temp, sz);
-        temp += sz; sz = 0;
+  while (!Success_h && line_try-- > 0) {
+    if (image_sz > 0) {
+      size_t BUF_SIZE = 1024;
+      if ( image_data != NULL) {
+        uint8_t *p = image_data;
+        size_t sz = image_sz;
+        while ( p != NULL && sz) {
+          if ( sz >= BUF_SIZE) {
+            client.write( p, BUF_SIZE);
+            p += BUF_SIZE; sz -= BUF_SIZE;
+          } else {
+            client.write( p, sz);
+            p += sz; sz = 0;
+          }
+        }
       }
+
+      while ( client.connected() && !client.available()) delay(10);
+      if ( client.connected() && client.available() ) {
+        String resp = client.readString();
+        httpCode    = resp.substring(resp.indexOf(" ") + 1, resp.indexOf(" ", resp.indexOf(" ") + 1)).toInt();
+        Success_h   = (httpCode == 200);
+        Serial.println(resp);
+      }
+      delay(10);
     }
   }
-
-  while ( client.connected() && !client.available()) delay(10);
-  
-  if ( client.connected() && client.available() ) {
-    String resp = client.readStringUntil('\n');
-    httpCode    = resp.substring(resp.indexOf(" ") + 1, resp.indexOf(" ", resp.indexOf(" ") + 1)).toInt();
-    Success_h   = (httpCode == 200);
-    Serial.println(resp);
-  }
-
   client.stop();
 }
 
 
 
-
-// void send_slack(File *file) {
+// void send_slack(uint8_t* image_data) {
 //   const char* slack_bot_token = get_slack_bot_token();
 //   const char* oauth_token = get_oauth_token();
   
 //   HTTPClient http;
 //   String url = "https://slack.com/api/files.upload";
 
-//   const int capacity = JSON_OBJECT_SIZE(4);
+  
+
+//   const int capacity = JSON_OBJECT_SIZE(6);
 //   StaticJsonDocument<capacity> json_request;
 //   json_request["token"] = oauth_token;
 //   json_request["channel"] = "#t_mizushima";
 //   json_request["filename"] = "From M5stickV!";
 //   json_request["initial_comment"] = "From M5stickV!";
-//   json_request["file"] = file->read();
+//   json_request["file"] = image_data;
 
 //   http.begin(url);
 //   http.addHeader("Content-Type", "application/json");
@@ -98,6 +86,6 @@ void send_slack(uint8_t* image_data, size_t image_sz) {
 //   unsigned int responseCode = http.POST((uint8_t*)buffer, strlen(buffer));
 //   String payload = http.getString();
 
-//   // M5.Lcd.printf("status code: %d", responseCode);
-//   // M5.Lcd.printf("payload: %s", payload.c_str());
+//   M5.Lcd.printf("status code: %d", responseCode);
+//   M5.Lcd.printf("payload: %s", payload.c_str());
 // }
